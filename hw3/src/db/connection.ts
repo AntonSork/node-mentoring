a4759eb426674918e02cb805fc3ae5f691a223dc
Anton Udot <Anton_Udot@epam.com>
2022-01-04 14:11:23 +0200

    moved db url to env

--- hw3/src/db/connection.ts
+++ hw3/src/db/connection.ts
@@ -1,7 +1,6 @@
 import { Sequelize } from 'sequelize'
-import { CONFIG } from '../../config'
 
-export const sequelize = new Sequelize(CONFIG.DATABASE_URL, {
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   logging: false,
   dialectOptions: {
     ssl: {
